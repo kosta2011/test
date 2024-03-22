4104e43033129b4a258c0d527233a00e0cbab4e4
kosta <kosta4job@yandex.ru>
2024-03-22 15:01:22 +0300

    test

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,5 +1,8 @@
+import lessons.Lesson1;
+
 public class Calculator {
     public static int add(int a, int b) {
+        System.out.println(Lesson1.class.getSimpleName());
         return a + b;
     }
 }
