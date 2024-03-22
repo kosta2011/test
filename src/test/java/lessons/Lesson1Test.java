011f728b462359d3ce10af7331c20766061a7dd4
kosta <kosta4job@yandex.ru>
2024-03-22 14:58:04 +0300

    test

--- src/test/java/lessons/Lesson1Test.java
+++ src/test/java/lessons/Lesson1Test.java
@@ -12,36 +12,48 @@ class Lesson1Test {
     @DisplayName("testEvenNumber")
     public void testEvenNumber() {
         assertTrue(isEven(4));
+
+        System.out.println("test completed");
     }
 
     @Test
     @DisplayName("testOddNumber")
     public void testOddNumber() {
         assertFalse(isEven(5));
+
+        System.out.println("test completed");
     }
 
     @Test
     @DisplayName("testFactorial")
     public void testFactorial() {
         assertEquals(120, factorial(5));
+
+        System.out.println("test completed");
     }
 
     @Test
     @DisplayName("testFactorialOfZero")
     public void testFactorialOfZero() {
         assertEquals(1, factorial(0));
+
+        System.out.println("test completed");
     }
 
     @Test
     @DisplayName("testReverse")
     public void testReverse() {
         assertEquals("dlrow ,olleH", reverse("Hello, world"));
+
+        System.out.println("test completed");
     }
 
     @Test
     @DisplayName("testFindMax")
     public void testFindMax() {
         assertEquals(8, findMax(new int[]{1, 3, 7, 6, 8, 2}));
+
+        System.out.println("test completed");
     }
 
 }
