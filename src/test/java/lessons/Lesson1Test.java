017318e3377d2e67ed9cbcf8da3c319839f081dd
kosta <kosta4job@yandex.ru>
2024-03-22 14:55:41 +0300

    test

--- src/test/java/lessons/Lesson1Test.java
+++ src/test/java/lessons/Lesson1Test.java
@@ -1,37 +1,45 @@
 package lessons;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static lessons.Lesson1.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class Lesson1Test {
+
     @Test
+    @DisplayName("testEvenNumber")
     public void testEvenNumber() {
         assertTrue(isEven(4));
     }
 
     @Test
+    @DisplayName("testOddNumber")
     public void testOddNumber() {
         assertFalse(isEven(5));
     }
 
     @Test
+    @DisplayName("testFactorial")
     public void testFactorial() {
         assertEquals(120, factorial(5));
     }
 
     @Test
+    @DisplayName("testFactorialOfZero")
     public void testFactorialOfZero() {
         assertEquals(1, factorial(0));
     }
 
     @Test
+    @DisplayName("testReverse")
     public void testReverse() {
         assertEquals("dlrow ,olleH", reverse("Hello, world"));
     }
 
     @Test
+    @DisplayName("testFindMax")
     public void testFindMax() {
         assertEquals(8, findMax(new int[]{1, 3, 7, 6, 8, 2}));
     }
